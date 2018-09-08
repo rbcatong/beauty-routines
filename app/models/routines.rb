aa0f811e7d30aefae177323940f7b7b3e4c51aa9
Rebecca Tong <rtong@buffalo.edu>
2018-09-08 17:55:49 +0000

    added migration for routines

--- app/models/routines.rb
+++ app/models/routines.rb
@@ -1,5 +1,5 @@
 class Routine < ActiveRecord::Base
 
-belongs_to :user
+  belongs_to :user
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ActiveRecord::Base
-
-has_many :routines
-
-has_secure_password
-
+  has_many :routines
+  has_secure_password
 end
