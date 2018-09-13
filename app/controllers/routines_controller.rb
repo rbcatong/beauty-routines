29a51cf04d82a72a0a2a9606692fcf43ec0c2a11
Rebecca Tong <rtong@buffalo.edu>
2018-09-13 17:54:56 +0000

    editing the create routes

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,7 +1,7 @@
 class RoutinesController < ApplicationController
 
   get '/routines' do
-    if logged_in
+    if logged_in?
       erb :'/routines/show'
     else
       erb :'/'
@@ -9,18 +9,24 @@ class RoutinesController < ApplicationController
   end
 
   get '/createroutine' do
-    if logged_in
-      if params[:routine_name] != "" && params[:routine_content] != ""
+    if logged_in?
       erb :'/routines/create'
     else
       erb :'/'
     end
-end
+  end
 
   post '/routines' do
     if logged_in?
-    @routine = current_user.routines.create(routine_name: params[:routine_name], routine_days: params[:routine_days], routine_content: params[:routine_content])
-
+      if params[:routine_name] != "" && params[:routine_content] != ""
+      @routine = current_user.routines.create(routine_name: params[:routine_name], routine_days: params[:routine_days], routine_content: params[:routine_content])
+      redirect "/routines/#{@routine/:slug}"
+    else
+      redirect '/createroutine'
+    end
+  else
+    redirect '/login'
+    end
   end
 
 end
--- app/models/routines.rb
+++ app/models/routines.rb
@@ -2,4 +2,16 @@ class Routine < ActiveRecord::Base
 
   belongs_to :user
 
+  def slug
+  if name
+    [name.parameterize].join("-")
+  end
+end
+
+def self.find_by_slug(slug)
+  Routine.all.find do |routine|
+  routine.slug == slug
+  end
+end
+
 end
