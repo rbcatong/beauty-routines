e0cead9b0c1e08fc868bede2d626ba20a0552566
Rebecca Tong <rtong@buffalo.edu>
2018-09-17 21:37:43 +0000

    edited the show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,6 @@ puts "Creating users..."
   User.create(id: 2, email: "jhake1@gmail.com", username: "jackh", password: "huangj")
 
   puts "Creating routines..."
-  Routine.create(id: 1, routine_content: "Wash face. Exfoliate", routine_days: "Sunday", routine_name: "skincare",user_id: 1)
-  Routine.create(id: 2, routine_content: "Sweep, mop,clean", routine_days: "Friday", routine_name: "cleaning",user_id: 1)
-  Routine.create(id: 3, routine_content: "cook vegetables, wash plates", routine_days: "Saturday", routine_name: "cooking",user_id: 2)
+  Routine.create(id: 1, routine_content: "Wash face. Exfoliate", routine_days: "Sunday", routine_name: "Skincare",user_id: 1)
+  Routine.create(id: 2, routine_content: "Sweep, mop,clean", routine_days: "Friday", routine_name: "Cleaning",user_id: 1)
+  Routine.create(id: 3, routine_content: "cook vegetables, wash plates", routine_days: "Saturday", routine_name: "Cooking",user_id: 2)
