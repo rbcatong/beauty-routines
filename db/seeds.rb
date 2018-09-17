d9ebb11d9770f78b17793921698726e4287d5e63
Rebecca Tong <rtong@buffalo.edu>
2018-09-17 21:24:11 +0000

    added seed data

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+puts "Creating users..."
+  User.create(id: 1, email: "rebeccaiscool@gmail.com", username: "rebeccat", password: "tongbecks")
+  User.create(id: 2, email: "jhake1@gmail.com", username: "jackh", password: "huangj")
+
+  puts "Creating routines..."
+  Routine.create(id: 1, routine_content: "Wash face. Exfoliate", routine_days: "Sunday", routine_name: "skincare",user_id: 1)
+  Routine.create(id: 2, routine_content: "Sweep, mop,clean", routine_days: "Friday", routine_name: "cleaning",user_id: 1)
+  Routine.create(id: 3, routine_content: "cook vegetables, wash plates", routine_days: "Saturday", routine_name: "cooking",user_id: 2)
