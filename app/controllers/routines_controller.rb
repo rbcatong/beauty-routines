83272247ad0002d62b7012c7263085a2c55ff667
Rebecca Tong <rtong@buffalo.edu>
2018-10-02 16:26:57 +0000

    added to edit flash message

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -19,16 +19,16 @@ class RoutinesController < ApplicationController
 
   post '/routines' do
     if logged_in?
-      if params[:routine_name] != "" && params[:routine_content] != "" && params[:routine_days] != " " 
-      @routine = current_user.routines.create(routine_name: params[:routine_name], routine_days: params[:routine_days], routine_content: params[:routine_content])
-      erb :'/routines/show'
+      if params[:routine_name] != "" && params[:routine_content] != "" && params[:routine_days] != " "
+        @routine = current_user.routines.create(routine_name: params[:routine_name], routine_days: params[:routine_days], routine_content: params[:routine_content])
+        erb :'/routines/show'
+      else
+        session[:error] = "There was an error while creating a routine, please fill in all blanks."
+        @error1 = session[:error]
+        erb :'/routines/create'
+      end
     else
-      session[:error] = "There was an error while creating a routine, please fill in all blanks."
-      @error1 = session[:error]
-      erb :'/routines/create'
-    end
-  else
-    redirect '/login'
+      redirect '/login'
     end
   end
 
@@ -39,24 +39,29 @@ class RoutinesController < ApplicationController
 
   patch '/routines/:id' do
     @routine = Routine.find_by_id(params[:id])
-    @routine.routine_name = params[:routine_name]
-    @routine.routine_days = params[:routine_days]
-    @routine.routine_content = params[:routine_content]
-    @routine.save
-    redirect '/routines'
-  end
-
-  delete '/routines/:id' do
-    if logged_in?
-      @routine = Routine.find_by_id(params[:id])
-      if @routine.user_id == current_user.id
-        @routine.destroy
+        @routine.routine_name = params[:routine_name]
+        @routine.routine_days = params[:routine_days]
+        @routine.routine_content = params[:routine_content]
+      if params[:routine_name] != "" && params[:routine_content] != "" && params[:routine_days] != " "
+        @routine.save
         redirect '/routines'
       else
-        redirect '/login'
+        session[:error] = "There was an error while creating a routine, please fill in all blanks."
+        @error2 = session[:error]
+        erb :'/routines/edit'
       end
     end
   end
 
 
+delete '/routines/:id' do
+  if logged_in?
+    @routine = Routine.find_by_id(params[:id])
+    if @routine.user_id == current_user.id
+      @routine.destroy
+      redirect '/routines'
+    else
+      redirect '/login'
+    end
   end
+end
